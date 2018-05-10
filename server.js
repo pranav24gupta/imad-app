cb2ba1bef219dd5807e8a9a29a984f2b8ed700a2
Pranav Gupta <pranavgupta42@gmail.com>
2018-05-10 16:35:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/article1',function(req,res)
 {
-    res.send("welcome to article 1");
+    res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 app.get('/article2',function(req,res)
 {
