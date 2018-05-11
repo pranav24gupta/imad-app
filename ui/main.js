a78d6b3fdb027c67c5c66d15c5054e69c3b07450
Pranav Gupta <pranavgupta42@gmail.com>
2018-05-11 11:28:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 
 element.innerHTML='new value';
+
+var img=document.getElementById('img');
+img.onclick= function(){
+    img.style.marginLeft='100px';
+
+    
+};
